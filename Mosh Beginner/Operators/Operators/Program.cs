44240ce6d436267a7d851905c4b5a134639645be
Wayne Harrison Jnr <WayneJnr@planetwayne.com>
2021-01-05 12:19:12 +0000

    Operators and Conversion
    Practice using operators and type conversions based on the c# for beginners course.

---
+++ Mosh Beginner/Operators/Operators/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 1;
+            var b = 2;
+            var c = 3;
+
+            Console.WriteLine(a + b);
+            Console.WriteLine((float)a / (float)b);
+            Console.WriteLine(a + b * c); //c# has operator precedence, it follows BODMAS by default.
+            Console.WriteLine(a > b); //Comparison operators are always true or false.
+            Console.WriteLine(a == b);
+            Console.WriteLine(a != b);
+            Console.WriteLine(!(a != b)); //Do not use double negative like this.
+            Console.WriteLine(c > b && c > a);
+            Console.WriteLine(c > b && c == a); //False, both need to be true to produce a true result.
+
+        }
+    }
+}
---
+++ Mosh Beginner/TypeCoversion/TypeCoversion/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TypeCoversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ii = 1;
+            byte bb = (byte) ii;
+            Console.WriteLine(bb);
+            string num = "1234";
+            int i = Convert.ToInt32(num);
+            Console.WriteLine(i);
+            //Try catch block for converting int to byte.
+            try
+            {
+                byte b = Convert.ToByte(num); //Fails because num is too large for byte. Conversion doesn't work.
+                Console.WriteLine(b);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Number too big to be converted to a byte.");
+            }
+
+            try
+            {
+                string str = "true";
+                bool b = Convert.ToBoolean(str);
+                Console.WriteLine(b);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            
+        }
+    }
+}
