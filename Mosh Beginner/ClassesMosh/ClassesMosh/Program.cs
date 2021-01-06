8c25597e9402fc63c29fdc2e57451ff3b39a8bf7
Wayne Harrison Jnr <WayneJnr@planetwayne.com>
2021-01-06 11:14:23 +0000

    Arrays and Classes Intro
    Introduction to arrays and classes.

---
+++ Mosh Beginner/ClassesMosh/ClassesMosh/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using ClassesMosh.Math;
+
+namespace ClassesMosh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wayne = new Person();
+            wayne.fName = "Wayne";
+            wayne.lName = "H";
+            wayne.Introduce();
+
+            Calculator calculator = new Calculator();
+            var result= calculator.Add(1, 2);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Mosh Beginner/ClassesMosh/ClassesMosh/person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassesMosh
+{
+    public class Person
+    {
+        public string fName;
+        public string lName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("My name is " + fName +" " + lName);
+        }
+    }
+}
