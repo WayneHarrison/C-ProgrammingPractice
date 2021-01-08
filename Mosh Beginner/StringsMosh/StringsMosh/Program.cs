19340679536bb5b3bd9b652f82e4135ab210329b
Wayne Harrison Jnr <WayneJnr@planetwayne.com>
2021-01-08 15:46:17 +0000

    Enums and string practice.

---
+++ Mosh Beginner/StringsMosh/StringsMosh/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace StringsMosh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstName = "Wayne";
+            var lastName = "H";
+
+            var fullName = firstName + " " + lastName;
+
+            var fName = string.Format("My name is {0} {1}", firstName, lastName);
+
+            var names = new string[3] {"John", "James", "Josef"};
+            var formattedNames = string.Join(",", names);
+            Console.WriteLine(formattedNames);
+
+            var text = @"Hi John
+Look at the following paths
+c:\folder1\folder2";
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ Mosh Beginner/enumsMosh/enumsMosh/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace enumsMosh
+{
+    public enum shippingMethods
+    {
+        RegAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var method = shippingMethods.Express;
+            Console.WriteLine((int)method);
+
+            var methodId = 3;
+            Console.WriteLine((shippingMethods)methodId);
+
+            Console.WriteLine(method.ToString());
+
+            var methodName = "Express";
+            var shippingMethod =  (shippingMethods)Enum.Parse(typeof(shippingMethods), methodName); //Parse a string to an enum
+        }
+    }
+}
