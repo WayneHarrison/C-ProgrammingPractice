400a0feb5a607907606b4654068ca3dd9e8d1098
Wayne Harrison Jnr <WayneJnr@planetwayne.com>
2021-01-26 12:16:48 +0000

    String Builders Example.

---
+++ Mosh Beginner/StringBuilders/StringBuilders/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace StringBuilders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // It is possible to daisy chain builders too.
+            var builder = new StringBuilder();
+            builder.Append('-', 10);
+            builder.AppendLine();
+            builder.Append("Header");
+            builder.AppendLine();
+            builder.Append('-', 10);
+
+            builder.Replace('-', '+');
+
+            builder.Remove(0, 10);
+
+            builder.Insert(0, new string('-', 10));
+            Console.WriteLine(builder);
+
+            //eg
+            var builder2 = new StringBuilder();
+            builder2
+                .Append('-', 10)
+                .AppendLine()
+                .Append("Header")
+                .AppendLine()
+                .Append('-', 10)
+                .Replace('-', '+')
+                .Remove(0, 10)
+                .Insert(0, new string('-', 10));
+            Console.WriteLine(builder2);
+            Console.WriteLine("First Char: " + builder2[0]);
+        }
+    }
+}
