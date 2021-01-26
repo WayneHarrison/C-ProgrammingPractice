4fd92f4c2191ade74a29a11d8bcb0853a747dc1a
Wayne Harrison Jnr <WayneJnr@planetwayne.com>
2021-01-26 12:29:18 +0000

    Text Summaries Example.

---
+++ Mosh Beginner/TextSummaries/TextSummaries/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TextSummaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence = "This is going to be a really really really really really really really long sentence.";
+            var summary = StringUtility.Summarize(sentence);
+            Console.WriteLine(summary);
+
+        }
+
+    }
+}
---
+++ Mosh Beginner/TextSummaries/TextSummaries/StringUtility.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextSummaries
+{
+    public class StringUtility
+    {
+        public static string Summarize(string text, int maxLen = 20)
+        {
+
+
+            if (text.Length < maxLen)
+            {
+                return text;
+            }
+
+            var words = text.Split(' ');
+            var totalChar = 0;
+            var summaryWords = new List<string>();
+
+            foreach (var word in words)
+            {
+                summaryWords.Add(word);
+                totalChar += word.Length + 1;
+                if (totalChar > maxLen)
+                {
+                    break;
+                }
+            }
+
+
+            return String.Join(" ", summaryWords) + "...";
+        }
+
+    }
+}
