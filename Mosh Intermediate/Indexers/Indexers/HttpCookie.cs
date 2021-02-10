f2cdd16456bf6a35ae39e13fbe8cad0509509b60
Wayne Harrison Jnr <WayneJnr@planetwayne.com>
2021-02-10 12:13:42 +0000

    Indexers

---
+++ Mosh Intermediate/Indexers/Indexers/HttpCookie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Indexers
+{
+    public class HttpCookie
+    {
+        private readonly Dictionary<string, string> _cookieDictionary;
+        public DateTime ExpiryDateTime { get; set; }
+
+        public HttpCookie()
+        {
+            _cookieDictionary = new Dictionary<string, string>();
+        }
+
+        public string this[string key]
+        {
+            get => _cookieDictionary[key];
+            set => _cookieDictionary[key] = value;
+        }
+    }
+}
