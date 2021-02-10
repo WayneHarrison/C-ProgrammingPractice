39b2831c43bef46b7e5ba94910851196f215784b
Wayne Harrison Jnr <WayneJnr@planetwayne.com>
2021-02-10 12:12:39 +0000

    Properties and Access Modifiers.

---
+++ Mosh Intermediate/Properties/Properties/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person(new DateTime(1997, 3, 30));
+            Console.WriteLine(person.Age);
+        }
+    }
+}
